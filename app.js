c1e27eb2d68b3d954bc61294e074780c37e428b2
Sebastian Langguth <langguth.sebastian@gmail.com>
2018-05-18 12:46:38 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+// mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://sebastian:merbas@ds021299.mlab.com:21299/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
